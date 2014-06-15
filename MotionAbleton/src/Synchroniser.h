c3b5284598cc03be9ad6c7f35edc02ecd925a670
Seb Lee-Delisle <sebleedelisle@gmail.com>
2014-06-15 15:13:04 +0100

    Add derived data to git ignore

---
+++ MotionAbleton/src/Synchroniser.h
@@ -0,0 +1,14 @@
+//
+//  Synchroniser.h
+//  MotionAbleton
+//
+//  Created by Seb Lee-Delisle on 15/06/2014.
+//
+//
+
+#ifndef MotionAbleton_Synchroniser_h
+#define MotionAbleton_Synchroniser_h
+
+
+
+#endif
